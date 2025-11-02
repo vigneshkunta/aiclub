f330d20bed202e358948752f9a2883e9c9703240
vignesh-yadav <vigneshyadav.dev@gmail.com>
2025-11-02 21:17:13 +0530

    fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { Provider } from "react-redux";
@@ -9,22 +8,23 @@ import { store, persistor } from "../redux/store";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Inter, Fira_Code } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const firaCode = Fira_Code({
+  variable: "--font-fira-code",
   subsets: ["latin"],
 });
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${firaCode.variable} antialiased`}
       >
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
